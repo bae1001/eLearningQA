c82036db97aad9233aaa03a92649d71070e2320b
RobertoArastiBlanco <rab1002@alu.ubu.es>
2022-04-11 18:15:17 +0200

    Fixed some bugs and smells #48

--- Project/prototipo/src/main/java/es/ubu/lsi/RegistryIO.java
+++ Project/prototipo/src/main/java/es/ubu/lsi/RegistryIO.java
@@ -1,6 +1,5 @@
 package es.ubu.lsi;
 
-import org.apache.catalina.LifecycleState;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -9,22 +8,25 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class RegistryIO {
     private static final Logger LOGGER = LogManager.getLogger();
+    private static final String barra=File.separator;
+    private static final String carpeta="registry";
+
+    private RegistryIO() {
+        throw new IllegalStateException("Utility class");
+    }
 
     public static void guardarResultados(String url, String user, String courseid, AnalysisSnapshot instantanea){
-        try {
-            File f1 = new File("registry/"+url);
-            File f2 = new File("registry/"+url+"/"+user);
-            FileWriter pw = new FileWriter("registry/"+url+"/"+user+"/"+courseid+".csv",true);
+        try (FileWriter pw = new FileWriter(carpeta+barra+url+barra+user+barra+courseid+".csv",true);){
+            File f1 = new File(carpeta+barra+url);
+            File f2 = new File(carpeta+barra+url+barra+user);
             f1.mkdir();
             f2.mkdir();
             pw.append(instantanea.toString());
             pw.flush();
-            pw.close();
         }catch (Exception e){
             LOGGER.error("exception", e);
         }
@@ -32,7 +34,7 @@ public class RegistryIO {
 
     public static List<AnalysisSnapshot> leerCSV(String url, String user, String courseid){
         List<AnalysisSnapshot> registros=new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader("registry/"+url+"/"+user+"/"+courseid+".csv"))){
+        try (BufferedReader br = new BufferedReader(new FileReader(carpeta+barra+url+barra+user+barra+courseid+".csv"))){
             String linea = br.readLine();
             String[] atributos;
             while (linea != null) {
@@ -49,9 +51,9 @@ public class RegistryIO {
 
 
 
-    public static String generarGrafico(String url,String user,String courseid){return "";}
+    public static String generarGrafico(String url,String user,String courseid){return url+user+courseid;}
 
-    public static String generarGrafico(String url,String user){return "";}
+    public static String generarGrafico(String url,String user){return url+user;}
 
-    public static String informeGlobal(String url,String user){return "";}
+    public static String informeGlobal(String url,String user){return user+url;}
 }
