f7f6f0dc6d60770b677a30d12dc092ce64d252ba
bae1001 <93576892+bae1001@users.noreply.github.com>
2024-04-27 19:16:57 +0200

    feat: Add messages to the test of the SessionService class #29

--- Project/prototipo/src/test/java/es/ubu/lsi/SessionServiceTest.java
+++ Project/prototipo/src/test/java/es/ubu/lsi/SessionServiceTest.java
@@ -1,5 +1,6 @@
 package es.ubu.lsi;
 
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -12,17 +13,17 @@ public class SessionServiceTest {
     private static final String host = "https://school.moodledemo.net";
     private static final String username = "teacher";
     private static final String password = "moodle";
-    private static String sskey;
+    private static SessionService sessionService;
 
     @BeforeAll
     public static void BeforeAll() throws IOException {
-        SessionService sessionService = SessionService.getInstance();
-        sskey = sessionService.getSSkey(username, password, host);
+        sessionService = SessionService.getInstance(username, password, host);
 
     }
 
     @Test
     void getSSkey() {
-        assertNotNull(sskey);
+        assertNotNull(sessionService.getSSKey(host), "The retrieved sskey should not be null");
+        assertNotEquals("The retrieved sskey should not be empty", "", sessionService.getSSKey(host));
     }
 }
